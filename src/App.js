612e635204de38d589164a29a8f26a01b0138f7a
Md. Mazharul Islam <mipiyas07@gmail.com>
2023-03-28 19:37:56 +0600

    Create Component, return HTML from a component

--- src/App.js
+++ src/App.js
@@ -1,43 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 
-const number = 5555;
-const singer = {name: 'Dr. Mahfuj',job: 'singer'}
-const singer2 = {name: 'Eva Rahman',job: 'singer2'}
-
-const singerStyle = {
-  color: 'purple',
-  backgroundColor:'white',
-}
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>JSX</h1>
-        <div className="container">
-          <h3>hello dude React. how are you</h3>
-        </div>
-        <div className="music">
-          <p>Name: {2222+1101+ number}</p>
-          <p style={singerStyle}>Name: {singer.name} {singer.job}</p>
-          <p style={{ color: 'aqua',backgroundColor:'yellow'}}>Name: {singer2.name} {singer2.job}</p>
-        </div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <h5>New Component, YAY</h5>
+      <p>rock mama react mama</p>
+      <Friend></Friend>
+      <Friend></Friend>
+      <Friend></Friend>
     </div>
   );
+}
+
+function Person(){
+  return (
+    <div className='person'>
+      <h1>Sakib al Hasan</h1>
+      <p>Profession: Cricket</p>
+    </div>
+  )
+}
+
+function Friend(){
+  return (
+    <div className='container'>
+        <h3>Name: Ajay Devgun</h3>
+        <p>job: fighting</p>
+    </div>
+  )
 }
 
 export default App;
